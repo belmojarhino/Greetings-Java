3e3f69aa53884ad81d4ec7b7380d7b599f0a7f98
Moisés Vilas Boas <moisesboas@gmail.com>
2024-05-04 14:09:05 -0300

    feat: conditionals

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    // How to declare a new variable.
+        // How to declare a new variable.
         int integerNumber = 42;
         byte bitNumber = 42;
         short shortNumber = 4200;
@@ -12,6 +12,18 @@ public class Main {
         String stringStrictedTyped = "Moisés";
         var stringTypedByInference = "Moisés";
         var numberTypedByInference = 42;
+
+
+        // Conditionals
+        var age = 30;
+        if (age > 27) {
+            System.out.println("You're approaching 30");
+        } else if (age < 25) {
+            System.out.println("You're still young");
+        } else {
+            System.out.println("You are an alien my partner");
+        }
+
 
     }
 }
